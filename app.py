96ebcdb2a98e0eaced114cc46cb976970c50aab9
merouane <bennaceurmerouane@gmail.com>
2023-05-01 06:31:25 +0200

    rapport

--- app.py
+++ app.py
@@ -14,236 +14,129 @@ from pyspark.ml.classification import RandomForestClassifier, GBTClassifier, Log
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator, BinaryClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+
+# Définir le schéma de la  DataFrame
+schema = StructType([
+    StructField("Age", IntegerType(), True),
+    StructField("Attrition", StringType(), True),
+    StructField("BusinessTravel", StringType(), True),
+    StructField("DailyRate", IntegerType(), True),
+    StructField("Department", StringType(), True),
+    StructField("DistanceFromHome", IntegerType(), True),
+    StructField("Education", IntegerType(), True),
+    StructField("EducationField", StringType(), True),
+    StructField("EmployeeCount", IntegerType(), True),
+    StructField("EmployeeNumber", IntegerType(), True),
+    StructField("EnvironmentSatisfaction", IntegerType(), True),
+    StructField("Gender", StringType(), True),
+    StructField("HourlyRate", IntegerType(), True),
+    StructField("JobInvolvement", IntegerType(), True),
+    StructField("JobLevel", IntegerType(), True),
+    StructField("JobRole", StringType(), True),
+    StructField("JobSatisfaction", IntegerType(), True),
+    StructField("MaritalStatus", StringType(), True),
+    StructField("MonthlyIncome", IntegerType(), True),
+    StructField("MonthlyRate", IntegerType(), True),
+    StructField("NumCompaniesWorked", IntegerType(), True),
+    StructField("Over18", StringType(), True),
+    StructField("OverTime", StringType(), True),
+    StructField("PercentSalaryHike", IntegerType(), True),
+    StructField("PerformanceRating", IntegerType(), True),
+    StructField("RelationshipSatisfaction", IntegerType(), True),
+    StructField("StandardHours", IntegerType(), True),
+    StructField("StockOptionLevel", IntegerType(), True),
+    StructField("TotalWorkingYears", IntegerType(), True),
+    StructField("TrainingTimesLastYear", IntegerType(), True),
+    StructField("WorkLifeBalance", IntegerType(), True),
+    StructField("YearsAtCompany", IntegerType(), True),
+    StructField("YearsInCurrentRole", IntegerType(), True),
+    StructField("YearsSinceLastPromotion", IntegerType(), True),
+    StructField("YearsWithCurrManager", IntegerType(), True),
+])
+
+def preprocess_data(filename):
+    data = spark.read.csv(filename, header=True, inferSchema=True)
+    data_cleaned = data
+    numerical_columns = ["Age", "DailyRate", "DistanceFromHome", "Education", "EnvironmentSatisfaction", "HourlyRate", "JobInvolvement", "JobLevel", "JobSatisfaction", "MonthlyIncome", "MonthlyRate", "NumCompaniesWorked", "PercentSalaryHike", "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel", "TotalWorkingYears", "TrainingTimesLastYear", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole", "YearsSinceLastPromotion", "YearsWithCurrManager"]
+    categorical_columns = ["BusinessTravel", "Department", "EducationField", "Gender", "JobRole", "MaritalStatus", "OverTime"]
+    for column in numerical_columns:
+        Q1 = data.approxQuantile(column, [0.25], 0.05)[0]
+        Q3 = data.approxQuantile(column, [0.75], 0.05)[0]
+        IQR = Q3 - Q1
+        lower_range = Q1 - 1.5 * IQR
+        upper_range = Q3 + 1.5 * IQR
+        data_cleaned = data_cleaned.filter((col(column) >= lower_range) & (col(column) <= upper_range))
+    stages = []
+    for column in categorical_columns:
+        indexer = StringIndexer(inputCol=column, outputCol=f"{column}Index")
+        encoder = OneHotEncoder(inputCol=f"{column}Index", outputCol=f"{column}Vec")
+        stages += [indexer, encoder]
+    for column in numerical_columns:
+        assembler = VectorAssembler(inputCols=[column], outputCol=f"{column}Vec")
+        scaler = MinMaxScaler(inputCol=f"{column}Vec", outputCol=f"{column}Scaled")
+        stages += [assembler, scaler]
+    pipeline = Pipeline(stages=stages)
+    data_prepared = pipeline.fit(data_cleaned).transform(data_cleaned)
+    input_cols = [f"{col}Scaled" for col in numerical_columns] + [f"{col}Vec" for col in categorical_columns]
+    assembler = VectorAssembler(inputCols=input_cols, outputCol="features")
+    data_features = assembler.transform(data_prepared)
+    indexer = StringIndexer(inputCol="Attrition", outputCol="label")
+    data_features = indexer.fit(data_features).transform(data_features)
+    selector = ChiSqSelector(numTopFeatures=10, featuresCol="features", outputCol="selectedFeatures", labelCol="label")
+    selected_data = selector.fit(data_features).transform(data_features)
+    attrition_counts = selected_data.groupBy("label").count().collect()
+    minority_count = min(attrition_counts, key=lambda x: x["count"])["count"]
+    majority_count = max(attrition_counts, key=lambda x: x["count"])["count"]
+    balancing_ratio = minority_count / majority_count
+    majority_label = max(attrition_counts, key=lambda x: x["count"])["label"]
+    minority_label = min(attrition_counts, key=lambda x: x["count"])["label"]
+    majority_data = selected_data.filter(col("label") == majority_label)
+    minority_data = selected_data.filter(col("label") == minority_label)
+    majority_data_downsampled = majority_data.sample(withReplacement=False, fraction=balancing_ratio, seed=42)
+    balanced_data = majority_data_downsampled.union(minority_data)
+
+    return balanced_data, majority_data_downsampled, minority_data
+
+def preprocess_input(data, majority_data_downsampled, minority_data):
+    data_cleaned = data
+    numerical_columns = ["Age", "DailyRate", "DistanceFromHome", "Education", "EnvironmentSatisfaction", "HourlyRate", "JobInvolvement", "JobLevel", "JobSatisfaction", "MonthlyIncome", "MonthlyRate", "NumCompaniesWorked", "PercentSalaryHike", "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel", "TotalWorkingYears", "TrainingTimesLastYear", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole", "YearsSinceLastPromotion", "YearsWithCurrManager"]
+    categorical_columns = ["BusinessTravel", "Department", "EducationField", "Gender", "JobRole", "MaritalStatus", "OverTime"]
+    for column in numerical_columns:
+        Q1 = data.approxQuantile(column, [0.25], 0.05)[0]
+        Q3 = data.approxQuantile(column, [0.75], 0.05)[0]
+        IQR = Q3 - Q1
+        lower_range = Q1 - 1.5 * IQR
+        upper_range = Q3 + 1.5 * IQR
+        data_cleaned = data_cleaned.filter((col(column) >= lower_range) & (col(column) <= upper_range))
+    stages = []
+    for column in categorical_columns:
+        indexer = StringIndexer(inputCol=column, outputCol=f"{column}Index")
+        assembler = VectorAssembler(inputCols=[f"{column}Index"], outputCol=f"{column}Vec")
+        stages += [indexer, assembler]
+    for column in numerical_columns:
+        assembler = VectorAssembler(inputCols=[column], outputCol=f"{column}Vec")
+        scaler = MinMaxScaler(inputCol=f"{column}Vec", outputCol=f"{column}Scaled")
+        stages += [assembler, scaler]
+    pipeline = Pipeline(stages=stages)
+    data_prepared = pipeline.fit(data_cleaned).transform(data_cleaned)
+    input_cols = [f"{col}Scaled" for col in numerical_columns] + [f"{col}Vec" for col in categorical_columns]
+    assembler = VectorAssembler(inputCols=input_cols, outputCol="features")
+    data_features = assembler.transform(data_prepared)
+    indexer = StringIndexer(inputCol="Attrition", outputCol="label")
+    data_features = indexer.fit(data_features).transform(data_features)
+    selector = ChiSqSelector(numTopFeatures=10, featuresCol="features", outputCol="selectedFeatures", labelCol="label")
+    selected_data = selector.fit(data_features).transform(data_features)
+    balanced_data = majority_data_downsampled.union(minority_data)
+
+    return balanced_data
 
 
-
-# Création de l'application Spark avec le nom PAE - Prédiction Attrition Employés - Merouane Bennaceur V1.0
 spark = SparkSession.builder.appName("PAE - Prédiction Attrition Employés - Merouane Bennaceur V1.0").getOrCreate()
-
-# Charger les données
-data = spark.read.csv("HR-Employee-Attrition.csv", header=True, inferSchema=True)
-
-# Afficher les 10 premières lignes
-print("\n\n")
-print("Affichage des 10 premières lignes")
-data.show(10)
-
-# Afficher le schéma
-print("\n\n")
-print("Affichage du schéma")
-data.printSchema()
-
-# Statistiques descriptives
-print("\n\n")
-print("Statistiques descriptives")
-data.describe().show()
-print("\n\n")
-data_summary = data.describe().toPandas()
-print(data_summary)
-
-# Vérification des valeurs manquantes
-# Identifiecation des colonnes avec des valeurs manquantes
-print("\n\n")
-print("Valeurs manquantes par colonnes")
-data.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in data.columns]).show()
-
-
-# Analyse des distributions de variables
-# Exemple avec age
-print("\n\n")
-print("Histogramme de distribution de la variable age")
-col_name = "age"
-col_data = data.select(col_name).toPandas()
-sns.histplot(col_data[col_name], kde=True)
-plt.show()
-
-
-# Examiner les corrélations entre les variables et visualiser les relations
-# Création d'un vecteur avec les colonnes numériques
-vector_col = "corr_features"
-assembler = VectorAssembler(inputCols=["Age", "DailyRate"], outputCol=vector_col)
-data_vector = assembler.transform(data).select(vector_col)
-
-# Calculer la matrice de corrélation
-matrix = Correlation.corr(data_vector, vector_col).collect()[0][0]
-correlation_matrix = matrix.toArray().tolist()
-print("\n\n")
-print("Matrice de corrélation entre Age et DailyRate")
-print(correlation_matrix)
-
-# Exemple pour un nuage de points entre deux variables numériques (Age, DailyRate)
-x_col = "Age"
-y_col = "DailyRate"
-scatter_data = data.select(x_col, y_col).toPandas()
-sns.scatterplot(data=scatter_data, x=x_col, y=y_col)
-print("\n\n")
-print("Nuage de points de corrélation entre Age et DailyRate")
-plt.show()
-
-# On va supprimer les valeurs aberrantes les colonnes numériques en utilisant l'intervalle interquartile (IQR).
-
-# Liste des colonnes numériques
-numerical_columns = ["Age", "DailyRate", "DistanceFromHome", "Education", "EnvironmentSatisfaction", "HourlyRate", "JobInvolvement", "JobLevel", "JobSatisfaction", "MonthlyIncome", "MonthlyRate", "NumCompaniesWorked", "PercentSalaryHike", "PerformanceRating", "RelationshipSatisfaction", "StockOptionLevel", "TotalWorkingYears", "TrainingTimesLastYear", "WorkLifeBalance", "YearsAtCompany", "YearsInCurrentRole", "YearsSinceLastPromotion", "YearsWithCurrManager"]
-
-data_cleaned = data
-
-for column in numerical_columns:
-    Q1 = data.approxQuantile(column, [0.25], 0.05)[0]
-    Q3 = data.approxQuantile(column, [0.75], 0.05)[0]
-    IQR = Q3 - Q1
-    lower_range = Q1 - 1.5 * IQR
-    upper_range = Q3 + 1.5 * IQR
-
-    data_cleaned = data_cleaned.filter((col(column) >= lower_range) & (col(column) <= upper_range))
-
-# Affichage dataset nettoyé
-print("\n\n")
-print("Affichage dataset nettoyé")
-data_clean.show()
-
-# Normalisation de toutes les colonnes numériques et transformation des colonnes discrettes en variables numériques (one-hot encoding).
-
-# Liste des colonnes discrétes
-categorical_columns = ["BusinessTravel", "Department", "EducationField", "Gender", "JobRole", "MaritalStatus", "OverTime"]
-
-# Stages pour le pipeline
-stages = []
-
-# One-hot encoding pour les colonnes catégorielles
-for column in categorical_columns:
-    indexer = StringIndexer(inputCol=column, outputCol=f"{column}Index")
-    encoder = OneHotEncoder(inputCol=f"{column}Index", outputCol=f"{column}Vec")
-    stages += [indexer, encoder]
-
-# VectorAssembler et MinMaxScaler pour les colonnes numériques
-for column in numerical_columns:
-    assembler = VectorAssembler(inputCols=[column], outputCol=f"{column}Vec")
-    scaler = MinMaxScaler(inputCol=f"{column}Vec", outputCol=f"{column}Scaled")
-    stages += [assembler, scaler]
-
-# Créer et appliquer un pipeline
-pipeline = Pipeline(stages=stages)
-data_prepared = pipeline.fit(data_clean).transform(data_clean)
-
-# Affichage dataset préparé
-print("\n\n")
-print("Affichage dataset préparé")
-data_prepared.show()
-
-# Utilisation méthode Chi-Squared pour sélectionner les fonctionnalités les plus pertinentes pour le modèle. 
-# pour sélectionner les 10 fonctionnalités les + importantes parmi les colonnes discrétes et numériques prétraitées.
-
-# Combinez toutes les colonnes d'entités en un seul vecteur
-input_cols = [f"{col}Scaled" for col in numerical_columns] + [f"{col}Vec" for col in categorical_columns]
-assembler = VectorAssembler(inputCols=input_cols, outputCol="features")
-data_features = assembler.transform(data_prepared)
-
-# Indexez la colonne Attrition (cible)
-indexer = StringIndexer(inputCol="Attrition", outputCol="label")
-data_features = indexer.fit(data_features).transform(data_features)
-
-# Utilisation ChiSqSelector pour sélectionner les fonctionnalités les plus pertinentes
-selector = ChiSqSelector(numTopFeatures=10, featuresCol="features", outputCol="selectedFeatures", labelCol="label")
-selected_data = selector.fit(data_features).transform(data_features)
-selected_data = selected_data.select("features","selectedFeatures", "label")
-
-# Affichage dataset avec les fonctionnalités pertinentes
-print("\n\n")
-print("Affichage dataset avec les fonctionnalités pertinentes")
-selected_data.show()
-
-# Calculez le taux d'équilibrage
-attrition_counts = selected_data.groupBy("label").count().collect()
-minority_count = min(attrition_counts, key=lambda x: x["count"])["count"]
-majority_count = max(attrition_counts, key=lambda x: x["count"])["count"]
-balancing_ratio = minority_count / majority_count
-
-# Équilibrez les données
-majority_label = max(attrition_counts, key=lambda x: x["count"])["label"]
-minority_label = min(attrition_counts, key=lambda x: x["count"])["label"]
-
-majority_data = selected_data.filter(col("label") == majority_label)
-minority_data = selected_data.filter(col("label") == minority_label)
-
-majority_data_downsampled = majority_data.sample(withReplacement=False, fraction=balancing_ratio, seed=42)
-balanced_data = majority_data_downsampled.union(minority_data)
-
-print("\n\n")
-print("Affichage dataset équilibré")
-balanced_data.show()
-
+balanced_data, majority_data_downsampled, minority_data = preprocess_data("HR-Employee-Attrition.csv")
 train_data, test_data = balanced_data.randomSplit([0.8, 0.2], seed=42)
-
-# Random Forest
-rf = RandomForestClassifier(featuresCol="selectedFeatures", labelCol="label", numTrees=100)
-rf_model = rf.fit(train_data)
-
-
-# Gradient Boosting
-gbt = GBTClassifier(featuresCol="selectedFeatures", labelCol="label", maxIter=100)
-gbt_model = gbt.fit(train_data)
-
-
-# Logistic Regression:
 lr = LogisticRegression(featuresCol="selectedFeatures", labelCol="label", maxIter=100)
-lr_model = lr.fit(train_data)
-
-# Decision Tree:
-dt = DecisionTreeClassifier(featuresCol="selectedFeatures", labelCol="label")
-dt_model = dt.fit(train_data)
-
-# Fonction pour évaluer les modèles
-def evaluate_model(model, test_data):
-    predictions = model.transform(test_data)
-    evaluator = BinaryClassificationEvaluator()
-    roc = evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})
-    predictionAndLabels = predictions.select("prediction", "label").rdd.map(lambda p: (float(p[0]), float(p[1])))
-    metrics = MulticlassMetrics(predictionAndLabels)
-    precision = metrics.precision(1.0)
-    recall = metrics.recall(1.0)
-    f1_score = metrics.fMeasure(1.0)
-    return roc, precision, recall, f1_score
-
-# Évaluation des modèles
-models = [("Random Forest", rf_model), ("Gradient Boosting", gbt_model), ("Logistic Regression", lr_model), ("Decision Tree", dt_model)]
-
-for model_name, model in models:
-    roc, precision, recall, f1_score = evaluate_model(model, test_data)
-    print(f"{model_name}:")
-    print(f"  Precision: {precision}")
-    print(f"  Recall: {recall}")
-    print(f"  F1 Score: {f1_score}")
-    print(f"  ROC: {roc}\n")
-    
-    
-# Pour optimiser les modèles on peut utiliser la validation croisée avec un ensemble de paramètres à tester. 
-# Voici un exemple avec la régression logistique car c'est elle qui a obtenu les meilleurs évaluations
-
-# Optimisation du modèle avec validation croisée et recherche sur grille pour la régression logistique
-paramGrid = ParamGridBuilder() \
-    .addGrid(lr.regParam, [0.1, 0.01, 0.001]) \
-    .addGrid(lr.fitIntercept, [True, False]) \
-    .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0]) \
-    .build()
-
-cross_val = CrossValidator(estimator=lr,
-                           estimatorParamMaps=paramGrid,
-                           evaluator=BinaryClassificationEvaluator(),
-                           numFolds=5)
-
-cv_model = cross_val.fit(train_data)
-best_lr_model = cv_model.bestModel
-
-# Évaluer le modèle optimisé
-roc, precision, recall, f1_score = evaluate_model(best_lr_model, test_data)
-print("Optimized Logistic Regression:")
-print(f"  Precision: {precision}")
-print(f"  Recall: {recall}")
-print(f"  F1 Score: {f1_score}\n")
-print(f"  Area Under ROC: {roc}\n")
-
-
-
+best_lr_model = lr.fit(train_data)
 
 app = Flask(__name__)
 es = Elasticsearch("http://elasticsearch:9200")
@@ -251,11 +144,17 @@ es = Elasticsearch("http://elasticsearch:9200")
 @app.route("/predict", methods=["POST"])
 def predict():
     input_data = request.get_json()
-    # Convertir les données d'entrée en DataFrame Spark
-    input_df = spark.createDataFrame([input_data])
-    # Préparer les données d'entrée en utilisant le pipeline
-    input_prepared = pipeline.fit(input_df).transform(input_df)
-    input_features = assembler.transform(input_prepared)
+    input_data = input_data["data"]
+    # EXEMPLE
+    # input_data = "34,No,Travel_Rarely,628,Research & Development,8,3,Medical,1,2068,2,Male,82,4,2,Laboratory Technician,3,Married,4404,10228,2,Y,No,12,3,1,80,0,6,3,4,4,3,1,2"
+    input_list = input_data.split(",")
+    input_list = [int(x) if x.isdigit() else x for x in input_list]
+
+    # Créer un dataframe Spark à partir du schéma défini plus haut
+    input_df = spark.createDataFrame([input_list], schema=schema)
+
+    # Préparer les données d'entrée en utilisant le pipeline et extraire les features
+    input_features = preprocess_input(input_df, majority_data_downsampled, minority_data)
     # Appliquer le modèle optimisé (par exemple, best_lr_model) aux données d'entrée
     prediction = best_lr_model.transform(input_features)
     # Convertir la prédiction en réponse JSON
